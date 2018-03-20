a85ccb7da04c35857871fdc07c8e94b9a6e648fd
Alan <alan.masriera@gmail.com>
2018-03-20 09:56:27 -0300

    Trabajando con cuentas

--- Contabilidad/Contabilidad/Form1.Designer.vb
+++ Contabilidad/Contabilidad/Form1.Designer.vb
@@ -24,20 +24,21 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.TVCuentas = New System.Windows.Forms.TreeView()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.BModificarNodo = New System.Windows.Forms.Button()
         Me.TBID = New System.Windows.Forms.TextBox()
         Me.TBNombre = New System.Windows.Forms.TextBox()
         Me.TBPadre = New System.Windows.Forms.TextBox()
-        Me.TBNum1 = New System.Windows.Forms.TextBox()
-        Me.TBNum2 = New System.Windows.Forms.TextBox()
-        Me.TBNum3 = New System.Windows.Forms.TextBox()
-        Me.TBNum4 = New System.Windows.Forms.TextBox()
-        Me.TBNum5 = New System.Windows.Forms.TextBox()
-        Me.TBNum6y7 = New System.Windows.Forms.TextBox()
+        Me.TBNum = New System.Windows.Forms.TextBox()
         Me.BGuardarModificacion = New System.Windows.Forms.Button()
         Me.BCancelarModificacion = New System.Windows.Forms.Button()
+        Me.TBNum5 = New System.Windows.Forms.TextBox()
+        Me.TBLvl = New System.Windows.Forms.TextBox()
+        Me.TBNum3 = New System.Windows.Forms.TextBox()
+        Me.TBNum6y7 = New System.Windows.Forms.TextBox()
+        Me.TBNum4 = New System.Windows.Forms.TextBox()
+        Me.BAgregar = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -55,15 +56,15 @@ Partial Class Form1
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(12, 72)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(439, 281)
+        Me.DataGridView1.Size = New System.Drawing.Size(344, 281)
         Me.DataGridView1.TabIndex = 1
         '
-        'TreeView1
+        'TVCuentas
         '
-        Me.TreeView1.Location = New System.Drawing.Point(471, 23)
-        Me.TreeView1.Name = "TreeView1"
-        Me.TreeView1.Size = New System.Drawing.Size(250, 373)
-        Me.TreeView1.TabIndex = 2
+        Me.TVCuentas.Location = New System.Drawing.Point(384, 17)
+        Me.TVCuentas.Name = "TVCuentas"
+        Me.TVCuentas.Size = New System.Drawing.Size(250, 373)
+        Me.TVCuentas.TabIndex = 2
         '
         'Button2
         '
@@ -76,9 +77,9 @@ Partial Class Form1
         '
         'BModificarNodo
         '
-        Me.BModificarNodo.Location = New System.Drawing.Point(782, 23)
+        Me.BModificarNodo.Location = New System.Drawing.Point(856, 17)
         Me.BModificarNodo.Name = "BModificarNodo"
-        Me.BModificarNodo.Size = New System.Drawing.Size(363, 53)
+        Me.BModificarNodo.Size = New System.Drawing.Size(170, 53)
         Me.BModificarNodo.TabIndex = 4
         Me.BModificarNodo.Text = "Modificar Cuenta"
         Me.BModificarNodo.UseVisualStyleBackColor = True
@@ -86,7 +87,7 @@ Partial Class Form1
         'TBID
         '
         Me.TBID.Enabled = False
-        Me.TBID.Location = New System.Drawing.Point(782, 106)
+        Me.TBID.Location = New System.Drawing.Point(663, 100)
         Me.TBID.Name = "TBID"
         Me.TBID.Size = New System.Drawing.Size(100, 20)
         Me.TBID.TabIndex = 5
@@ -94,7 +95,7 @@ Partial Class Form1
         'TBNombre
         '
         Me.TBNombre.Enabled = False
-        Me.TBNombre.Location = New System.Drawing.Point(920, 106)
+        Me.TBNombre.Location = New System.Drawing.Point(801, 100)
         Me.TBNombre.Name = "TBNombre"
         Me.TBNombre.Size = New System.Drawing.Size(100, 20)
         Me.TBNombre.TabIndex = 6
@@ -102,62 +103,22 @@ Partial Class Form1
         'TBPadre
         '
         Me.TBPadre.Enabled = False
-        Me.TBPadre.Location = New System.Drawing.Point(1045, 106)
+        Me.TBPadre.Location = New System.Drawing.Point(926, 100)
         Me.TBPadre.Name = "TBPadre"
         Me.TBPadre.Size = New System.Drawing.Size(100, 20)
         Me.TBPadre.TabIndex = 7
         '
-        'TBNum1
+        'TBNum
         '
-        Me.TBNum1.Enabled = False
-        Me.TBNum1.Location = New System.Drawing.Point(782, 168)
-        Me.TBNum1.Name = "TBNum1"
-        Me.TBNum1.Size = New System.Drawing.Size(19, 20)
-        Me.TBNum1.TabIndex = 8
-        '
-        'TBNum2
-        '
-        Me.TBNum2.Enabled = False
-        Me.TBNum2.Location = New System.Drawing.Point(807, 168)
-        Me.TBNum2.Name = "TBNum2"
-        Me.TBNum2.Size = New System.Drawing.Size(19, 20)
-        Me.TBNum2.TabIndex = 9
-        '
-        'TBNum3
-        '
-        Me.TBNum3.Enabled = False
-        Me.TBNum3.Location = New System.Drawing.Point(832, 168)
-        Me.TBNum3.Name = "TBNum3"
-        Me.TBNum3.Size = New System.Drawing.Size(19, 20)
-        Me.TBNum3.TabIndex = 10
-        '
-        'TBNum4
-        '
-        Me.TBNum4.Enabled = False
-        Me.TBNum4.Location = New System.Drawing.Point(857, 168)
-        Me.TBNum4.Name = "TBNum4"
-        Me.TBNum4.Size = New System.Drawing.Size(19, 20)
-        Me.TBNum4.TabIndex = 11
-        '
-        'TBNum5
-        '
-        Me.TBNum5.Enabled = False
-        Me.TBNum5.Location = New System.Drawing.Point(882, 168)
-        Me.TBNum5.Name = "TBNum5"
-        Me.TBNum5.Size = New System.Drawing.Size(19, 20)
-        Me.TBNum5.TabIndex = 12
-        '
-        'TBNum6y7
-        '
-        Me.TBNum6y7.Enabled = False
-        Me.TBNum6y7.Location = New System.Drawing.Point(907, 168)
-        Me.TBNum6y7.Name = "TBNum6y7"
-        Me.TBNum6y7.Size = New System.Drawing.Size(31, 20)
-        Me.TBNum6y7.TabIndex = 13
+        Me.TBNum.Enabled = False
+        Me.TBNum.Location = New System.Drawing.Point(663, 154)
+        Me.TBNum.Name = "TBNum"
+        Me.TBNum.Size = New System.Drawing.Size(137, 20)
+        Me.TBNum.TabIndex = 8
         '
         'BGuardarModificacion
         '
-        Me.BGuardarModificacion.Location = New System.Drawing.Point(782, 212)
+        Me.BGuardarModificacion.Location = New System.Drawing.Point(663, 206)
         Me.BGuardarModificacion.Name = "BGuardarModificacion"
         Me.BGuardarModificacion.Size = New System.Drawing.Size(363, 53)
         Me.BGuardarModificacion.TabIndex = 14
@@ -166,32 +127,82 @@ Partial Class Form1
         '
         'BCancelarModificacion
         '
-        Me.BCancelarModificacion.Location = New System.Drawing.Point(782, 282)
+        Me.BCancelarModificacion.Location = New System.Drawing.Point(663, 276)
         Me.BCancelarModificacion.Name = "BCancelarModificacion"
         Me.BCancelarModificacion.Size = New System.Drawing.Size(363, 25)
         Me.BCancelarModificacion.TabIndex = 15
         Me.BCancelarModificacion.Text = "Cancelar"
         Me.BCancelarModificacion.UseVisualStyleBackColor = True
         '
+        'TBNum5
+        '
+        Me.TBNum5.Enabled = False
+        Me.TBNum5.Location = New System.Drawing.Point(856, 180)
+        Me.TBNum5.Name = "TBNum5"
+        Me.TBNum5.Size = New System.Drawing.Size(19, 20)
+        Me.TBNum5.TabIndex = 12
+        '
+        'TBLvl
+        '
+        Me.TBLvl.Enabled = False
+        Me.TBLvl.Location = New System.Drawing.Point(856, 154)
+        Me.TBLvl.Name = "TBLvl"
+        Me.TBLvl.Size = New System.Drawing.Size(69, 20)
+        Me.TBLvl.TabIndex = 9
+        '
+        'TBNum3
+        '
+        Me.TBNum3.Enabled = False
+        Me.TBNum3.Location = New System.Drawing.Point(806, 180)
+        Me.TBNum3.Name = "TBNum3"
+        Me.TBNum3.Size = New System.Drawing.Size(19, 20)
+        Me.TBNum3.TabIndex = 10
+        '
+        'TBNum6y7
+        '
+        Me.TBNum6y7.Enabled = False
+        Me.TBNum6y7.Location = New System.Drawing.Point(881, 180)
+        Me.TBNum6y7.Name = "TBNum6y7"
+        Me.TBNum6y7.Size = New System.Drawing.Size(31, 20)
+        Me.TBNum6y7.TabIndex = 13
+        '
+        'TBNum4
+        '
+        Me.TBNum4.Enabled = False
+        Me.TBNum4.Location = New System.Drawing.Point(831, 180)
+        Me.TBNum4.Name = "TBNum4"
+        Me.TBNum4.Size = New System.Drawing.Size(19, 20)
+        Me.TBNum4.TabIndex = 11
+        '
+        'BAgregar
+        '
+        Me.BAgregar.Location = New System.Drawing.Point(663, 17)
+        Me.BAgregar.Name = "BAgregar"
+        Me.BAgregar.Size = New System.Drawing.Size(170, 53)
+        Me.BAgregar.TabIndex = 16
+        Me.BAgregar.Text = "Agregar cuenta"
+        Me.BAgregar.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1194, 442)
+        Me.ClientSize = New System.Drawing.Size(1047, 442)
+        Me.Controls.Add(Me.BAgregar)
         Me.Controls.Add(Me.BCancelarModificacion)
         Me.Controls.Add(Me.BGuardarModificacion)
         Me.Controls.Add(Me.TBNum6y7)
         Me.Controls.Add(Me.TBNum5)
         Me.Controls.Add(Me.TBNum4)
         Me.Controls.Add(Me.TBNum3)
-        Me.Controls.Add(Me.TBNum2)
-        Me.Controls.Add(Me.TBNum1)
+        Me.Controls.Add(Me.TBLvl)
+        Me.Controls.Add(Me.TBNum)
         Me.Controls.Add(Me.TBPadre)
         Me.Controls.Add(Me.TBNombre)
         Me.Controls.Add(Me.TBID)
         Me.Controls.Add(Me.BModificarNodo)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.TVCuentas)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -204,18 +215,19 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents TVCuentas As TreeView
     Friend WithEvents Button2 As Button
     Friend WithEvents BModificarNodo As Button
     Friend WithEvents TBID As TextBox
     Friend WithEvents TBNombre As TextBox
     Friend WithEvents TBPadre As TextBox
-    Friend WithEvents TBNum1 As TextBox
-    Friend WithEvents TBNum2 As TextBox
-    Friend WithEvents TBNum3 As TextBox
-    Friend WithEvents TBNum4 As TextBox
-    Friend WithEvents TBNum5 As TextBox
-    Friend WithEvents TBNum6y7 As TextBox
+    Friend WithEvents TBNum As TextBox
     Friend WithEvents BGuardarModificacion As Button
     Friend WithEvents BCancelarModificacion As Button
+    Friend WithEvents TBNum5 As TextBox
+    Friend WithEvents TBLvl As TextBox
+    Friend WithEvents TBNum3 As TextBox
+    Friend WithEvents TBNum6y7 As TextBox
+    Friend WithEvents TBNum4 As TextBox
+    Friend WithEvents BAgregar As Button
 End Class
---
+++ Contabilidad/Contabilidad/TreeManager.vb
@@ -0,0 +1,61 @@
+﻿Public Class TreeManager
+    Dim table As New DataTable
+    Dim tree As TreeView
+    Dim nodeList As List(Of TreeNode) = New List(Of TreeNode)
+
+    Public Sub GetTree(ByVal t As DataTable, ByVal tv As TreeView)
+
+        table = t
+        tree = tv
+        tree.Nodes.Clear()
+        table.PrimaryKey = New DataColumn() {table.Columns("Id")}
+        For Each row As DataRow In table.Rows
+            Dim node As New TreeNode
+            Dim nodeNum As String = GetNodeNum(row("id")).ToString
+            nodeNum = CompletarNuemracion(nodeNum)
+            node.Text = nodeNum + " - " + row("Nombre")
+            node.Tag = row("id")
+            addNode(node, row("ID_NodoPadre"))
+        Next
+    End Sub
+
+    Private Function GetNodeNum(ByVal id As Integer) As String
+        Dim row As DataRow
+        Dim parentID As Integer
+
+        row = table.Rows.Find(id)
+        parentID = row("ID_NodoPadre")
+        If parentID = 0 Then
+            Return row("Numeracion").ToString
+        Else
+            Return GetNodeNum(parentID).ToString + row("Numeracion").ToString
+        End If
+
+    End Function
+
+    Private Sub addNode(ByVal node As TreeNode, ByVal idParentNode As Integer)
+
+        If idParentNode = 0 Then
+            tree.Nodes.Add(node)
+        Else
+            GetNodeFromID(idParentNode, nodeList).Nodes.Add(node)
+        End If
+        nodeList.Add(node)
+    End Sub
+
+    Private Function GetNodeFromID(ByVal id As Integer, nodeList As List(Of TreeNode)) As TreeNode
+        For Each node As TreeNode In nodeList
+            If node.Tag = id Then
+                Return node
+            End If
+        Next
+    End Function
+
+    Private Function CompletarNuemracion(ByVal str As String)
+        While str.Length < 7
+            str = str + "0"
+        End While
+        Return str
+    End Function
+
+End Class
